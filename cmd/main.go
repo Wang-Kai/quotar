0a06abe9be7b396eb0d79061cd62dbe96c4b3b02
Steve <persistence201306@gmail.com>
2021-02-14 14:38:03 +0800

    use logrus library to add log info

--- cmd/main.go
+++ cmd/main.go
@@ -1,35 +1,33 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
 	"github.com/Wang-Kai/quotar/pb"
-	_ "github.com/Wang-Kai/quotar/pkg/conf"
+	"github.com/Wang-Kai/quotar/pkg/conf"
 	"github.com/Wang-Kai/quotar/pkg/svc"
 	_ "github.com/Wang-Kai/quotar/pkg/xfs"
-)
-
-const (
-	PORT = ":10013"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	lis, err := net.Listen("tcp", PORT)
+	port := fmt.Sprintf(":%d", conf.PORT)
+	lis, err := net.Listen("tcp", port)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
 	s := grpc.NewServer()
-
 	// for grpc debug
 	reflection.Register(s)
 	pb.RegisterQuotarServer(s, &svc.QuotarService{})
 
+	log.WithField("Port", port).Info("gRPC server running ...")
 	if err := s.Serve(lis); err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 }

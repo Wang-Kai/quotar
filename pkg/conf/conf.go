155ea0f3490b98c458f14093a8ae9e185749b367
Steve <persistence201306@gmail.com>
2021-02-14 15:12:59 +0800

    add PORT env variables

--- pkg/conf/conf.go
+++ pkg/conf/conf.go
@@ -2,23 +2,27 @@ package conf
 
 import (
 	"flag"
-	"log"
 
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
+
+	log "github.com/sirupsen/logrus"
 )
 
 /*
 	conf package will load all configuration, which will be used by service, including:
 
 	1. workspace, which is nfs export path, and xfs mount pointer
+	2. port, the port for gRPC server
 */
 
 var WORKSPACE string
+var PORT int
 
 func init() {
 	// set flag
 	flag.String("workspace", "/data", "nfs export path, and xfs mount pointer")
+	flag.Int("port", 10013, "the port for gRPC server")
 
 	// bind flag to pflag
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
@@ -28,11 +32,15 @@ func init() {
 	viper.BindPFlags(pflag.CommandLine)
 
 	// catch env variables
-	viper.AutomaticEnv()
-	if err := viper.BindEnv("WORKSPACE"); err != nil {
-		log.Fatal(err)
-	}
+	viper.BindEnv("PORT")
+	viper.BindEnv("WORKSPACE")
 
 	// set global variables
 	WORKSPACE = viper.GetString("WORKSPACE")
+	PORT = viper.GetInt("PORT")
+
+	log.WithFields(log.Fields{
+		"WORKSPACE": WORKSPACE,
+		"PORT":      PORT,
+	}).Info("Global variables catch by viper")
 }

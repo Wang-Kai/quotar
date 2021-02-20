b7e1dfc5c386e4eb04626a02d3b51884576a5f69
Steve <persistence201306@gmail.com>
2021-02-20 14:51:28 +0800

    add log

--- nfs-provisioner/provision-controller.go
+++ nfs-provisioner/provision-controller.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Wang-Kai/quotar/pb"
 	"github.com/kubernetes-sigs/sig-storage-lib-external-provisioner/controller"
 	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -25,16 +26,24 @@ func (n *nfsProvisioner) Provision(opt controller.ProvisionOptions) (*v1.Persist
 		Quota: quota,
 	}
 
+	log.WithFields(log.Fields{
+		"name":  createDirReq.Name,
+		"quota": createDirReq.Quota,
+	}).Info("Call RPC to create dir")
+
 	resp, err := QuotarClient.CreateDir(context.Background(), createDirReq)
 	if err != nil {
 		return nil, errors.Wrap(err, "call quotar to create dir")
 	}
+
+	log.WithField("path", resp.Path).Info("Create Dir response")
 
 	pv := &v1.PersistentVolume{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: opt.PVName,
 		},
 		Spec: v1.PersistentVolumeSpec{
+			AccessModes: opt.PVC.Spec.AccessModes,
 			Capacity: v1.ResourceList{
 				v1.ResourceName(v1.ResourceStorage): resourceQuantity,
 			},
@@ -54,6 +63,7 @@ func (n *nfsProvisioner) Delete(pv *v1.PersistentVolume) error {
 	req := &pb.DeleteDirReq{
 		Name: pv.Name,
 	}
+	log.WithField("name", req.Name).Info("Call RPC to delete dir")
 
 	_, err := QuotarClient.DeleteDir(context.Background(), req)
 	if err != nil {
